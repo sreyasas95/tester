1ed0136d75fe11bf14f48987544bc47f78d4ed6c
sreyasas95 <sreyasas95@gmail.com>
2025-09-20 11:29:08 +0200

    updated the documentation and added documentation for printer.

---
+++ R/printer.R
@@ -0,0 +1,20 @@
+#' Silly printer function
+#'
+#' @param r what you want in the second column
+#' @param x what you want in the first column
+#'
+#' @return A tibble
+#' @export
+#'
+#' @importFrom tibble data_frame
+#' @importFrom utils head
+#' @importFrom dplyr select
+#'
+#' @examples
+#' printer(x = rnorm(5), r = rnorm(5))
+printer = function(r,x){
+  x = tibble::tibble(x = x,r = r)
+  print(head(x))
+  return(x)
+}
+
